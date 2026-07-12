2b5e0149ffd8db97f52a8b3595337fc0266cff51
Ziad Tamer <225040@eru.edu.eg>
2026-07-12 21:29:39 +0300

    Add Custom Text For Field to Login View Body

---
+++ lib/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/utils/app_text_styles.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    required this.hintText,
+    this.suffixIcon,
+    required this.keyboardType,
+  });
+  final String hintText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: keyboardType,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyles.semiBold13.copyWith(color: Color(0xFF949D9E)),
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: Color(0xFFF9FAFA),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(4),
+      borderSide: BorderSide(color: Color(0xFFE6E9E9), width: 1),
+    );
+  }
+}
