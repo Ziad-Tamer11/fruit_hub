d8f3a02a15cfffba4915c677b2750af888a61d5a
Ziad Tamer <225040@eru.edu.eg>
2026-07-12 22:46:10 +0300

    Add password recovery text and login button to LoginViewBody

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 abstract class AppColors {
-  static const Color primaryColor = Color(0xff1F5E3B);
+  static const Color primaryColor = Color(0xFF1B5E37);
+  static const Color lightPrimaryColor = Color(0xFF2D9F5D);
   static const Color secondaryColor = Color(0xFFF4A91F);
 }
--- lib/feature/auth/presentation/view/widgets/login_view_body.dart
+++ lib/feature/auth/presentation/view/widgets/login_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/constants.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
+import 'package:fruit_hub/core/utils/app_text_styles.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/core/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -22,6 +25,38 @@ class LoginViewBody extends StatelessWidget {
               hintText: 'كلمة المرور',
               suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
               keyboardType: TextInputType.visiblePassword,
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'نسيت كلمة المرور؟',
+                style: TextStyles.semiBold13.copyWith(
+                  color: AppColors.lightPrimaryColor,
+                ),
+              ),
+            ),
+            SizedBox(height: 33),
+            CustomButton(onPressed: () {}, text: 'تسجيل دخول'),
+            SizedBox(height: 33),
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: 'لا تمتلك حساب؟ ',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: const Color(0xFF616A6B) /* Grayscale-400 */,
+                    ),
+                  ),
+                  TextSpan(
+                    text: 'قم بإنشاء حساب',
+                    style: TextStyles.semiBold16.copyWith(
+                      color: AppColors.primaryColor /* Green1-500 */,
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
             ),
           ],
         ),
