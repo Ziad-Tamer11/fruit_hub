6f1c55a2af176effe0e24e922df1d5ad369394e5
Ziad Tamer <225040@eru.edu.eg>
2026-07-12 05:41:25 +0300

    Refactor OnBoardingViewBody to use StatefulWidget for dynamic page tracking

--- lib/feature/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
+++ lib/feature/on_boarding/presentation/view/widgets/on_boarding_view_body.dart
@@ -5,15 +5,39 @@ import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/feature/on_boarding/presentation/view/widgets/on_boarding_page_view.dart';
 
-class OnBoardingViewBody extends StatelessWidget {
+class OnBoardingViewBody extends StatefulWidget {
   const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  late PageController pageController;
+  var currentPage = 0;
+
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(() {
+      setState(() {
+        currentPage = pageController.page!.round();
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Expanded(child: const OnBoardingPageView()),
-
         DotsIndicator(
           dotsCount: 2,
           position: 0,
