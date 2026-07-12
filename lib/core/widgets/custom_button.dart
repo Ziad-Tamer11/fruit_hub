e6dabbc086b8f5f582953e2d42b15dac21777d21
Ziad Tamer <225040@eru.edu.eg>
2026-07-12 07:11:18 +0300

    Add Font Style to CustomButton text

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
+import 'package:fruit_hub/core/utils/app_text_styles.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({super.key, required this.onPressed, required this.text});
@@ -14,7 +15,10 @@ class CustomButton extends StatelessWidget {
       height: 54,
       child: TextButton(
         onPressed: onPressed,
-        child: Text(text, style: const TextStyle(color: Colors.white)),
+        child: Text(
+          text,
+          style: TextStyles.bold16.copyWith(color: Colors.white),
+        ),
         style: TextButton.styleFrom(
           backgroundColor: AppColors.primaryColor,
           shape: RoundedRectangleBorder(
