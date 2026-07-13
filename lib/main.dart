4921027a7a6956daedbb232ae4f314a6240ede4d
Ziad Tamer <225040@eru.edu.eg>
2026-07-13 16:31:17 +0300

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helper/on_generate_routes.dart';
 import 'package:fruit_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/feature/splash/presentation/view/splash_view.dart';
+import 'package:fruit_hub/firebase_options.dart';
 import 'package:fruit_hub/generated/l10n.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Prefs.init();
   runApp(const FruitHub());
 }
