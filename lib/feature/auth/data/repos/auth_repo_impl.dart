6bea1308c657257bdf8d10a874989fd6a9c1361b
Ziad Tamer <225040@eru.edu.eg>
2026-07-13 17:33:43 +0300

    Implement Auth Repo create user with email and password

--- lib/feature/auth/data/repos/auth_repo_impl.dart
+++ lib/feature/auth/data/repos/auth_repo_impl.dart
@@ -1,15 +1,31 @@
 import 'package:dartz/dartz.dart';
+import 'package:fruit_hub/core/errors/exceptions.dart';
 import 'package:fruit_hub/core/errors/failure.dart';
+import 'package:fruit_hub/core/services/firebase_auth_service.dart';
+import 'package:fruit_hub/feature/auth/data/model/user_model.dart';
 import 'package:fruit_hub/feature/auth/domain/entities/user_entity.dart';
 import 'package:fruit_hub/feature/auth/domain/repos/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthRepoImpl({required this.firebaseAuthService});
   @override
   Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
     String email,
     String password,
-  ) {
-    // TODO: implement createUserWithEmailAndPassword
-    throw UnimplementedError();
+    String name,
+  ) async {
+    try {
+      var user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return right(UserModel.fromFirebaseUser(user));
+    } on CustomException catch (e) {
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      return left(ServerFailure('An error occured. Please try again later.'));
+    }
   }
 }
