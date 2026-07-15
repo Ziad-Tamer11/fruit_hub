135c7656a859917ba71b977a70f70455bd6d8f20
Ziad Tamer <225040@eru.edu.eg>
2026-07-15 03:05:21 +0300

    Create Abstract Database Service

---
+++ lib/core/services/firestore_service.dart
@@ -0,0 +1,12 @@
+import 'package:fruit_hub/core/services/database_service.dart';
+
+class FireStoreService implements DatabaseService {
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+}
