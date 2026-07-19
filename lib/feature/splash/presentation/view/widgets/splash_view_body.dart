affcebcc3de3e23cb1535ac48fc679fedd898d34
Ziad Tamer <225040@eru.edu.eg>
2026-07-19 03:24:12 +0300

    Navigate User to Home View Directly

--- lib/feature/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/view/widgets/splash_view_body.dart
@@ -3,9 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruit_hub/constants.dart';
+import 'package:fruit_hub/core/services/firebase_auth_service.dart';
 import 'package:fruit_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
 import 'package:fruit_hub/feature/auth/presentation/view/login_view.dart';
+import 'package:fruit_hub/feature/home/presentation/view/home_view.dart';
 import 'package:fruit_hub/feature/on_boarding/presentation/view/on_boarding_view.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -42,7 +44,12 @@ class _SplashViewBodyState extends State<SplashViewBody> {
     bool isOnBoardingViewSeen = Prefs.getBool(kIsOnBoardingViewSeen);
     Future.delayed(const Duration(seconds: 3), () {
       if (isOnBoardingViewSeen) {
-        Navigator.pushReplacementNamed(context, LoginView.routeName);
+        var isLoggedIn = FirebaseAuthService().isLoggedIn();
+        if (isLoggedIn) {
+          Navigator.pushReplacementNamed(context, HomeView.routeName);
+        } else {
+          Navigator.pushReplacementNamed(context, LoginView.routeName);
+        }
       } else {
         Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
       }
