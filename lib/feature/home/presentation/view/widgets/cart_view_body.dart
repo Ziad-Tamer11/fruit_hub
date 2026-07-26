a910c95f27e35bf08bac15bc0eebc42adf602919
Ziad Tamer <225040@eru.edu.eg>
2026-07-26 22:52:55 +0300

    Finish Cart Feature

--- lib/feature/home/presentation/view/widgets/cart_view_body.dart
+++ lib/feature/home/presentation/view/widgets/cart_view_body.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/constants.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
-import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/feature/home/presentation/manager/cart_cubit/cart_cubit.dart';
 import 'package:fruit_hub/feature/home/presentation/view/widgets/cart_header.dart';
 import 'package:fruit_hub/feature/home/presentation/view/widgets/cart_item_list.dart';
+import 'package:fruit_hub/feature/home/presentation/view/widgets/custom_cart_button.dart';
 
 class CartViewBody extends StatelessWidget {
   const CartViewBody({super.key});
@@ -51,11 +51,7 @@ class CartViewBody extends StatelessWidget {
           left: 16,
           right: 16,
           bottom: MediaQuery.sizeOf(context).height * .05,
-          child: CustomButton(
-            onPressed: () {},
-            text:
-                'الدفع  ${context.watch<CartCubit>().cartEntity.calculateTotalPrice()} جنيه',
-          ),
+          child: CustomCartButton(),
         ),
       ],
     );
