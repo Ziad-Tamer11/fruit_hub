fe651d66da24bde2df7517fbb0c770dda8b0163c
Ziad Tamer <225040@eru.edu.eg>
2026-07-26 19:18:29 +0300

    Finish Cart View Final Part

--- lib/feature/home/presentation/view/widgets/cart_view_body.dart
+++ lib/feature/home/presentation/view/widgets/cart_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/constants.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/feature/home/presentation/view/widgets/cart_header.dart';
 import 'package:fruit_hub/feature/home/presentation/view/widgets/cart_item_list.dart';
 
@@ -9,26 +10,36 @@ class CartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: kHorizontalPadding,
-                ),
-                child: buildCustomAppBar(context, title: 'السلة'),
+    return Stack(
+      children: [
+        CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: kHorizontalPadding,
+                    ),
+                    child: buildCustomAppBar(context, title: 'السلة'),
+                  ),
+                  const SizedBox(height: kTopPadding),
+                  CartHeader(),
+                  const SizedBox(height: 23),
+                ],
               ),
-              const SizedBox(height: kTopPadding),
-              CartHeader(),
-              const SizedBox(height: 23),
-            ],
-          ),
+            ),
+            SliverToBoxAdapter(child: CustomDivider()),
+            CartItemList(),
+            SliverToBoxAdapter(child: CustomDivider()),
+          ],
         ),
-        SliverToBoxAdapter(child: CustomDivider()),
-        CartItemList(),
-        SliverToBoxAdapter(child: CustomDivider()),
+        Positioned(
+          left: 16,
+          right: 16,
+          bottom: MediaQuery.sizeOf(context).height * .05,
+          child: CustomButton(onPressed: () {}, text: 'الدفع  120جنيه'),
+        ),
       ],
     );
   }
