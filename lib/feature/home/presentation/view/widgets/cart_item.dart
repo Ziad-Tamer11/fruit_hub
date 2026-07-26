0e65a615e46188fee4ebc31673d7c86229d1fef3
Ziad Tamer <225040@eru.edu.eg>
2026-07-26 21:57:51 +0300

    Remove Cart Item Done

--- lib/feature/home/presentation/view/widgets/cart_item.dart
+++ lib/feature/home/presentation/view/widgets/cart_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
 import 'package:fruit_hub/core/utils/app_text_styles.dart';
 import 'package:fruit_hub/core/widgets/custom_network_image.dart';
 import 'package:fruit_hub/feature/home/domain/entities/cart_item_entity.dart';
+import 'package:fruit_hub/feature/home/presentation/manager/cart_cubit/cart_cubit.dart';
 import 'package:fruit_hub/feature/home/presentation/view/widgets/cart_item_acion_buttons.dart';
 
 class CartItem extends StatelessWidget {
@@ -37,7 +39,11 @@ class CartItem extends StatelessWidget {
                     ),
                     Spacer(),
                     GestureDetector(
-                      onTap: () {},
+                      onTap: () {
+                        context.read<CartCubit>().removeCartItem(
+                          cartItemEntity,
+                        );
+                      },
                       child: SvgPicture.asset(Assets.imagesTrash),
                     ),
                   ],
