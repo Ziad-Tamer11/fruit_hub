217546191d5f4aa8e571451eec475ca6aa39277f
Ziad Tamer <225040@eru.edu.eg>
2026-07-27 23:03:41 +0300

    Fix issue with shipping section state

--- lib/feature/checkout/presentation/view/widgets/shipping_section.dart
+++ lib/feature/checkout/presentation/view/widgets/shipping_section.dart
@@ -10,10 +10,12 @@ class ShippingSection extends StatefulWidget {
   State<ShippingSection> createState() => _ShippingSectionState();
 }
 
-class _ShippingSectionState extends State<ShippingSection> {
+class _ShippingSectionState extends State<ShippingSection>
+    with AutomaticKeepAliveClientMixin {
   int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     var orderEntity = context.read<OrderEntity>();
     return Column(
       children: [
@@ -44,4 +46,7 @@ class _ShippingSectionState extends State<ShippingSection> {
       ],
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
