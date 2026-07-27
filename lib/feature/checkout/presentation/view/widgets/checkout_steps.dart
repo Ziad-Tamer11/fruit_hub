0c74a9c17399540672551afd8e36bc408d69a713
Ziad Tamer <225040@eru.edu.eg>
2026-07-27 03:00:55 +0300

    Create Step Item

--- lib/feature/checkout/presentation/view/widgets/checkout_steps.dart
+++ lib/feature/checkout/presentation/view/widgets/checkout_steps.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fruit_hub/feature/checkout/presentation/view/widgets/in_active_step_item.dart';
+import 'package:fruit_hub/feature/checkout/presentation/view/widgets/step_item.dart';
 
 class CheckoutSteps extends StatelessWidget {
   const CheckoutSteps({super.key});
@@ -9,9 +9,10 @@ class CheckoutSteps extends StatelessWidget {
     return Row(
       children: List.generate(getSteps().length, (index) {
         return Expanded(
-          child: InActiveStepItem(
+          child: StepItem(
             text: getSteps()[index],
             index: index.toString(),
+            isActive: true,
           ),
         );
       }),
---
+++ lib/feature/checkout/presentation/view/widgets/step_item.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/feature/checkout/presentation/view/widgets/active_step_item.dart';
+import 'package:fruit_hub/feature/checkout/presentation/view/widgets/in_active_step_item.dart';
+
+class StepItem extends StatelessWidget {
+  const StepItem({
+    super.key,
+    required this.text,
+    required this.index,
+    required this.isActive,
+  });
+  final String text, index;
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedCrossFade(
+      firstChild: ActiveStepItem(text: text),
+      secondChild: InActiveStepItem(index: index, text: text),
+      crossFadeState: isActive
+          ? CrossFadeState.showSecond
+          : CrossFadeState.showFirst,
+      duration: Duration(microseconds: 300),
+    );
+  }
+}
