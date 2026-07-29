23541d3a05bf17c71b7afa23987529fbd66a720d
Ziad Tamer <225040@eru.edu.eg>
2026-07-29 20:45:04 +0300

    Process payment part one

--- lib/feature/checkout/domain/entities/paypal_payment_entity/paypal_payment_entity.dart
+++ lib/feature/checkout/domain/entities/paypal_payment_entity/paypal_payment_entity.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:fruit_hub/feature/checkout/domain/entities/order_entity.dart';
 
 import 'amount.dart';
 import 'item_list.dart';
@@ -10,15 +11,19 @@ class PaypalPaymentEntity extends Equatable {
 
   const PaypalPaymentEntity({this.amount, this.description, this.itemList});
 
-  factory PaypalPaymentEntity.fromEntity(
-    PaypalPaymentEntity paypalPaymentEntity,
-  ) {
+  factory PaypalPaymentEntity.fromEntity(OrderEntity orderEntity) {
     return PaypalPaymentEntity(
-      amount: paypalPaymentEntity.amount,
-      description: paypalPaymentEntity.description,
-      itemList: paypalPaymentEntity.itemList,
+      amount: AmountEntity.fromEntity(orderEntity),
+      description: 'Pamyment Description',
+      itemList: ItemList.fromEntity(orderEntity.cartEntity.cartItems),
     );
   }
+
+  toJson() => {
+    'amount': amount?.toJson(),
+    'description': description,
+    'itemList': itemList?.toJson(),
+  };
   @override
   List<Object?> get props => [amount, description, itemList];
 }
--- lib/feature/checkout/presentation/view/widgets/checkout_view_body.dart
+++ lib/feature/checkout/presentation/view/widgets/checkout_view_body.dart
@@ -3,9 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:fruit_hub/constants.dart';
 import 'package:fruit_hub/core/helper/show_message_bar.dart';
+import 'package:fruit_hub/core/utils/app_keys.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/feature/checkout/domain/entities/order_entity.dart';
+import 'package:fruit_hub/feature/checkout/domain/entities/paypal_payment_entity/paypal_payment_entity.dart';
 import 'package:fruit_hub/feature/checkout/presentation/view/widgets/checkout_steps.dart';
 import 'package:fruit_hub/feature/checkout/presentation/view/widgets/checkout_steps_page_view.dart';
 
@@ -107,46 +109,17 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
   }
 
   void _processPayment(BuildContext context) {
+    var orderEntity = context.read<OrderEntity>();
+    PaypalPaymentEntity paypalPaymentEntity = PaypalPaymentEntity.fromEntity(
+      orderEntity,
+    );
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (BuildContext context) => PaypalCheckoutView(
           sandboxMode: true,
-          clientId: "",
-          secretKey: "",
-          transactions: const [
-            {
-              "amount": {
-                "total": '70',
-                "currency": "USD",
-                "details": {
-                  "subtotal": '70',
-                  "shipping": '0',
-                  "shipping_discount": 0,
-                },
-              },
-              "description": "The payment transaction description.",
-              // "payment_options": {
-              //   "allowed_payment_method":
-              //       "INSTANT_FUNDING_SOURCE"
-              // },
-              "item_list": {
-                "items": [
-                  {
-                    "name": "Apple",
-                    "quantity": 4,
-                    "price": '5',
-                    "currency": "USD",
-                  },
-                  {
-                    "name": "Pineapple",
-                    "quantity": 5,
-                    "price": '10',
-                    "currency": "USD",
-                  },
-                ],
-              },
-            },
-          ],
+          clientId: kPaypalClientId,
+          secretKey: kPaypalSecretKey,
+          transactions: [paypalPaymentEntity.toJson()],
           note: "Contact us for any questions on your order.",
           onSuccess: (Map params) async {
             print("onSuccess: $params");
